c00252cd8292084979f19a23425c823db09d609b
Noah <125345369+noahandjoe@users.noreply.github.com>
2023-08-21 14:29:41 +0000

    persistence to disk

--- sdb_comment.c
+++ sdb_comment.c
@@ -45,21 +45,23 @@ typedef struct
     uint32_t id;
     char username[COLUMN_USERNAME_SIZE + 1];
     char email[COLUMN_EMAIL_SIZE + 1];
-} Row;
+} Row;  //数据库中的一行数据，包含 ID、用户名和邮箱
 
 typedef struct
 {
-    StatementType type;
-    Row row_to_insert;
-} Statement;
+    StatementType type; //语句类型
+    Row row_to_insert;  //要插入的行数据
+} Statement;    //一个 SQL 语句
 
 typedef struct
 {
-    char *buffer;
-    size_t buffer_length; // typedef unsigned long size_t
-    ssize_t input_length; // typedef long ssize_t
-} InputBuffer;
+    char *buffer;   //缓冲区指针
+    size_t buffer_length; // 缓冲区长度
+    ssize_t input_length; // 输入长度
+} InputBuffer;  //从用户输入读取的数据
 
+
+//创建一个新的输入缓冲区，初始化相应的字段
 InputBuffer *new_input_buffer()
 {
     InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
@@ -83,23 +85,23 @@ const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;          // 4
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE; // 36
 
 // a table structure that points to pages of rows
-const uint32_t PAGE_SIZE = 4096;
-#define TABLE_MAX_PAGES 100
-const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
-const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+const uint32_t PAGE_SIZE = 4096;    //一页大小
+#define TABLE_MAX_PAGES 100         //总页数
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE; //每页的行数
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES; //总行数
 
 typedef struct
 {
-    int file_descriptor;
-    uint32_t file_length;
-    void *pages[TABLE_MAX_PAGES];
-} Pager;
+    int file_descriptor;    //文件描述符
+    uint32_t file_length;   //文件长度
+    void *pages[TABLE_MAX_PAGES];   //页面数组
+} Pager;    //对数据库文件的管理
 
 typedef struct
 {
-    Pager *pager;
-    uint32_t num_rows;
-} Table;
+    Pager *pager;   //Pager指针
+    uint32_t num_rows;  //数据的行数
+} Table;    //一个数据库表
 
 void serialize_row(Row *source, void *destination)
 {
@@ -117,6 +119,7 @@ void deserialize_row(void *source, Row *destination)
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+//根据页号从文件中读取页数据，实现简单的页面缓存
 void* get_page(Pager* pager, uint32_t page_num)
 {
     if(page_num > TABLE_MAX_PAGES){
@@ -125,41 +128,48 @@ void* get_page(Pager* pager, uint32_t page_num)
         exit(EXIT_FAILURE);
     }
 
+    //检查指定页面是否已经缓存在内存中
     if(pager->pages[page_num] == NULL){
         //Cache miss. Allocate memory and load from file.
         void* page = malloc(PAGE_SIZE);
         uint32_t num_pages = pager->file_length / PAGE_SIZE;
 
-        //We might save a partial page at the end of the file
+        //如果文件大小不是页面大小的整数倍，
+        //说明可能有不满一页的数据，因此需要额外的一个页面
         if(pager->file_length % PAGE_SIZE){
             num_pages += 1;
         }
 
         if(page_num <= num_pages){
+            //将文件指针定位到指定页面的开始位置
             lseek(pager->file_descriptor, page_num*PAGE_SIZE, SEEK_SET);
+            //文件中读取一页数据到刚刚分配的内存块
             ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
             if(bytes_read == -1){
                 printf("Error reading file: %d\n", errno);
                 exit(EXIT_FAILURE);
             }
         }
+        //将读取的页面数据指针存储到 Pager 结构中的页面数组中
         pager->pages[page_num] = page;
     }
+    //返回页面数据的指针
     return pager->pages[page_num];
 }
 
-// figure out where to read/write in memory for a particular row
+// 根据行号计算出相应的页号和字节偏移，以便在内存中找到行数据的位置
 void *row_slot(Table *table, uint32_t row_num)
 {
+    //计算所在页面的页号
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = get_page(table->pager, page_num);
+    void* page = get_page(table->pager, page_num);  //获取页面数据指针
 
-    uint32_t row_offset = row_num % ROWS_PER_PAGE;
-    uint32_t byte_offset = row_offset * ROW_SIZE;
-    return page + byte_offset;
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;  //页面中的行偏移
+    uint32_t byte_offset = row_offset * ROW_SIZE;   //页面中的字节偏移
+    return page + byte_offset;  //返回一个指向所需行数据的指针
 }
 
-// open the database file and keep track of its size
+// 函数打开数据库文件，初始化数据，返回一个 Pager 结构
 Pager *pager_open(const char *filename)
 {
     int fd = open(filename,
@@ -185,10 +195,11 @@ Pager *pager_open(const char *filename)
     return pager;
 }
 
-// opening a connection to the database
+// 打开数据库并返回一个 Table 结构
 Table *db_open(const char *filename)
 {
     Pager *pager = pager_open(filename);
+    //行数
     uint32_t num_rows = pager->file_length / ROW_SIZE;
     
     Table *table = malloc(sizeof(Table));
@@ -198,6 +209,7 @@ Table *db_open(const char *filename)
     return table;
 }
 
+//将内存中的页数据写回文件
 void pager_flush(Pager* pager, uint32_t page_num, uint32_t size){
     if(pager->pages[page_num] == NULL){
         printf("Tried to flush null page\n");
@@ -219,22 +231,25 @@ void pager_flush(Pager* pager, uint32_t page_num, uint32_t size){
     }
 }
 
+//关闭数据库，同时释放内存中的页数据
 void db_close(Table* table){
-    Pager* pager = table->pager;
-    uint32_t num_full_pages = table->num_rows/ROWS_PER_PAGE;
+    Pager* pager = table->pager;    //获取指向数据库管理器 Pager 的指针
+    uint32_t num_full_pages = table->num_rows/ROWS_PER_PAGE;    //满页的数量
 
     for(uint32_t i = 0; i<num_full_pages; i++){
+        //检查当前页面是否为空
         if(pager->pages[i] == NULL){
             continue;
         }
-        pager_flush(pager, i, PAGE_SIZE);
+        pager_flush(pager, i, PAGE_SIZE);   //将当前页面数据写回磁盘
         free(pager->pages[i]);
         pager->pages[i] = NULL;
     }
 
+    //处理不满一页的数据
     uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
     if(num_additional_rows > 0){
-        uint32_t page_num = num_full_pages;
+        uint32_t page_num = num_full_pages; //最后一个满页面的页号
         if(pager->pages[page_num] != NULL){
             pager_flush(pager, page_num, num_additional_rows*ROW_SIZE);
             free(pager->pages[page_num]);
@@ -259,6 +274,7 @@ void db_close(Table* table){
     free(table);
 }
 
+//打印行数据
 void print_row(Row *row)
 {
     printf("(%u, %s, %s)\n", row->id, row->username, row->email);
@@ -389,6 +405,7 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
 PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement)
 {
     statement->type = STATEMENT_INSERT;
+    //提取数据
     char *keyword = strtok(input_buffer->buffer, " ");
     char *id_string = strtok(NULL, " ");
     char *username = strtok(NULL, " ");
@@ -448,7 +465,7 @@ ExecuteResult execute_insert(Statement *statement, Table *table)
     Row *row_to_insert = &(statement->row_to_insert);
     void *row_memory = row_slot(table, table->num_rows);
     memset(row_memory, 0, ROW_SIZE);
-    serialize_row(row_to_insert, row_memory);
+    serialize_row(row_to_insert, row_memory); 
     table->num_rows += 1;
 
     return EXECUTE_SUCCESS;
